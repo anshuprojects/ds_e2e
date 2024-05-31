456a339e86d76ea603702aa66e7e989cc8ae570b
Beauty Khan <anshu.rubul@gmail.com>
2024-05-31 09:33:58 +0530

    Added Exception

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from src.mlproject.logger import logging
+from src.mlproject.exception import CustomerException
+import sys
 
 if __name__ == "__main__":
     logging.info("The execution has started")
